4f97eca61123d49468100398836b97b9142f63b6
Juan Sifontez <juan.sifontez.dev@gmail.com>
2023-05-12 10:00:39 -0400

    Add styles to TodoCounter component

--- src/components/TodoCounter.jsx
+++ src/components/TodoCounter.jsx
@@ -1,7 +1,11 @@
+import '../styles/TodoCounter.css'
+
 function TodoCounter ({ total, completed }) {
   return (
-    <h1>Has completado {completed} de {total} TODOS</h1>
-  )
+    <h1 className='TodoCounter'>
+      Has completado <span>{completed}</span> de <span>{total}</span> TODOS
+    </h1>
+  );
 }
 
 export default TodoCounter
