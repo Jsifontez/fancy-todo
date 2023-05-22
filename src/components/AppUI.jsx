bebbf03b771594ff349005c32f72231b4a287720
Juan Sifontez <juan.sifontez.dev@gmail.com>
2023-05-22 02:06:58 -0400

    refactor: Remove context consumer sintax and use hook instead

--- src/components/AppUI.jsx
+++ src/components/AppUI.jsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react'
 import TodoCounter from '../components/TodoCounter'
 import TodoSearch from '../components/TodoSearch'
 import TodoList from '../components/TodoList'
@@ -9,40 +10,36 @@ import '../styles/App.css'
 import { TodoContext } from './TodoContext'
 
 function AppUI () {
+  const {
+    isLoading,
+    isError,
+    filterTodos,
+    deleteTodo,
+    completeTodo
+  } = useContext(TodoContext)
   return (
     <>
       <TodoCounter />
       <TodoSearch />
 
-      {/* This is used to consume the context of react */}
-      <TodoContext.Consumer>
-        {({
-          isLoading,
-          isError,
-          filterTodos,
-          deleteTodo,
-          completeTodo
-        }) => (  
-          <TodoList>
-          {isLoading && <TodosLoading />}
-          {isError && <TodosError />}
-          {!isLoading && filterTodos.length === 0 && <p>Crea tu primer TODO</p>}
+      <TodoList>
+      {isLoading && <TodosLoading />}
+      {isError && <TodosError />}
+      {!isLoading && filterTodos.length === 0 && <p>Crea tu primer TODO</p>}
 
-          {(!isLoading &&
-            !isError &&
-            filterTodos.length > 0) &&
-            filterTodos.map((todo, index) => (
-              <TodoItem
-                key={todo.text}
-                text={todo.text}
-                completed={todo.completed}
-                deleteTodo={() => deleteTodo(index)}
-                completeTodo={() => completeTodo(index)}
-              />
-            ))}
-          </TodoList>
-        )}
-      </TodoContext.Consumer>
+      {(!isLoading &&
+        !isError &&
+        filterTodos.length > 0) &&
+        filterTodos.map((todo, index) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            deleteTodo={() => deleteTodo(index)}
+            completeTodo={() => completeTodo(index)}
+          />
+        ))}
+      </TodoList>
 
       <TodoCreateButton />
     </>
